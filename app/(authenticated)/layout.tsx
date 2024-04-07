1d51bbe40c2c65c2109ff756a2beae177a1043bd
Siddharth Srinivasan <siddhuvaradan@gmail.com>
2024-04-07 23:21:28 +0530

    add header

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,4 +1,5 @@
 import { buttonVariants } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function AuthenticatedLayout({
@@ -9,19 +10,24 @@ export default function AuthenticatedLayout({
   return (
     <div>
       <header className="fixed top-0 left-0 right-0 shadow-md z-50 backdrop-blur-md">
-        <nav className="w-full p-2">
-          <Link
-            href="/dashboard"
-            className={buttonVariants({ variant: "ghost" })}
-          >
-            Home
-          </Link>
-          <Link
-            href="/my-profile"
-            className={buttonVariants({ variant: "ghost" })}
-          >
-            My profile
-          </Link>
+        <nav className="w-full p-2 flex justify-between">
+          <div>
+            <Link
+              href="/dashboard"
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              Home
+            </Link>
+            <Link
+              href="/my-profile"
+              className={buttonVariants({ variant: "ghost" })}
+            >
+              My profile
+            </Link>
+          </div>
+          <div className="mr-4">
+            <UserButton afterSignOutUrl="/" />
+          </div>
         </nav>
       </header>
       <div className="pt-16">{children}</div>
